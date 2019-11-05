8d2adc69101bf61e9bb5517d63a3c20b5eac8c00
pstilian <pstilian111@gmail.com>
2019-11-05 13:30:22 -0500

    Cleaning code

--- client2.c
+++ client2.c
@@ -70,7 +70,8 @@ void main()
     printf("connected client socket to the server socket \n");
 
     // This section takes a ready command from client in order to proceed when there are 2 players
-   printf("Select \"y\" if you are ready. Or \"n\" to exit.\n");
+    go = 1
+    printf("Select \"y\" if you are ready. Or \"n\" to exit.\n");
    while(go) {
       fgets(buffer, 1024, stdin);
       if(buffer[0] == 'n' || buffer[0] == 'N'){
