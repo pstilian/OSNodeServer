384274942c7e0674bfc28d32de33c9f944e4c00f
donaldc354 <donaldc354@gmail.com>
2019-11-12 14:27:06 -0500

    Completed the rungame function in server, added print board functions and applied a way to view the gameboard status after every move on both server and client side.

--- server.c
+++ server.c
@@ -8,105 +8,95 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<netdb.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
 #include<sys/shm.h>
-#include<sys/sem.h>
 #define PORTNUM  10101 /* the port number the server will listen to*/
 #define DEFAULT_PROTOCOL 0  /*constant for default protocol*/
-#define SHMKEY ( (key_t) 7890)
-#define SEMKEY ( (key_t) 400L)
-#define NSEMS 1
+#define SHMKEY ((key_t) 7890)
 
-// Defining Shared Memory
-typedef struct{
-  int numClients;
-  char letterBoard[36];
-  char valueBoard[36];
-  int selections; //used to determine when all letters have been selected
+typedef struct
+{
+    int scores[5];
+    int count;
 }shared_mem;
-shared_mem *sharedData;
-
-// Defining Semaphore union
-union{
-  int val;
-  struct semid_ds *buf;
-  ushort *array;
-} semctl_arg;
-
-static struct sembuf Wait   = {0,-1,0};
-static struct sembuf Signal = {0,1,0};
-
-// Declared Functions
-void doprocessing (int sock);
-
-// Global Variables
-int numReady, status, semStatus, semid, semnum, semval;
+shared_mem *playerInfo;
 
 
-//--------------------------------Mainline Code---------------------------------
+
+
+//Initialized a matrix and testMap that needs ints
+
+char gameBoard[4][4]=       {{'A','B','C','D'},
+                             {'E','F','G','H'},
+                             {'I','J','K','L'},
+                             {'M','N','O','P'}};
+                             
+char gameBoardMap[4][4] =   {{'4','6','2','1'},
+                             {'0','6','2','3'},
+                             {'8','4','9','1'},
+                             {'6','6','8','9'}};
+
+int scoreMap[4][4] =        {{4,6,2,1},
+                             {0,6,2,3},
+                             {8,4,9,1},
+                             {6,6,8,9}};
+
+
+void printBoard(int i, int j);
+void rungame(int sock);
+void doprocessing (int sock, int pid);
+char buffer[256];
+
 int main( int argc, char *argv[] ) {
-    key_t key = 123; /* shared memory key */
-    int shmid;
-    char *shmadd;
-    char letters[36] = "a b c d\ne f g h\ni j k l\nm n o p\n";
+
+    //int client_size[6];
+    
     int sockfd, newsockfd, portno, clilen;
-    char buffer[1024];
     struct sockaddr_in serv_addr, cli_addr;
     int status, pid;
-    shmadd = (char *) 0;
+    /////////Added working on SharedMemory /////////
+    int shmid;
+    key_t key = 123; /*shared memory key*/
+    char* shmadd;
+    shmadd = (char*)0;
 
-    // Generating Shared Memory
-    if ((shmid = shmget (key, sizeof(int), IPC_CREAT | 0666)) < 0){
-   		perror ("shmget");
-   		exit (1);
-  	}
-  	if ((sharedData = (shared_mem *) shmat (shmid, shmadd, 0)) == (shared_mem *) -1){
-   		perror ("shmat");
-   		exit (0);
-  	}
 
-    // This line gets the semaphore
-	  semid = semget (SEMKEY, NSEMS, IPC_CREAT | 0666);
-    if (semid < 0){
-	     printf( "error in creating semaphore");
-	      exit (1);
+    if((shmid = shmget (SHMKEY, sizeof(int), IPC_CREAT | 0666)) < 0){
+        perror ("shmget");
+        exit (1);
     }
 
-    /* this sets the value of the semaphore semnum to semctl_arg.val*/
-    semnum = 0;
-    semctl_arg.val = 1;
-    status = semctl(semid, semnum, SETVAL, semctl_arg);
-
+    if ((playerInfo = (shared_mem *) shmat (shmid, shmadd, 0)) == (shared_mem *) -1){
+        perror ("shmat");
+        exit (0);
+    }
+    ////////////////End of SharedMemory setup /////////////////////
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM,DEFAULT_PROTOCOL );
     if (sockfd < 0) {
         perror("ERROR opening socket");
         exit(1);
     }
-
     /* Initialize socket structure */
     bzero((char *) &serv_addr, sizeof(serv_addr));
     portno = PORTNUM;
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(portno);
-
     /* Now bind the host address using bind() call.*/
-    status =  bind(sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr));
+    status =  bind(sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)); 
     if (status < 0) {
       perror("ERROR on binding");
       exit(1);
     }
-
-    /* Now Server starts listening clients wanting to connect. No more than 5
+    /* Now Server starts listening clients wanting to connect. No more than 5 
     clients allowed */
     listen(sockfd,5);
     int i = 0;
     clilen = sizeof(cli_addr);
-
-    // Manage Shared Memory Here
-    sharedData->numClients = 0;
-    strcpy(sharedData->letterBoard, letters);
-
+    /////////////////old////////////////
     while (1) {
        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr,  &clilen);
        if (newsockfd < 0) {
@@ -121,102 +111,95 @@ int main( int argc, char *argv[] ) {
         }
         if (pid == 0) {
          /* This is the client process */
+            
             close(sockfd);
-            doprocessing(newsockfd);
+            doprocessing(newsockfd, pid);
             exit(0);
         }
         else {
             close(newsockfd);
         }
     } /* end of while */
-
-    /* the next set of statements releases the shared memory */
-    if ((shmctl (shmid, IPC_RMID, (struct shmid_ds *) 0)) == -1){
-      perror ("shmctl");
-      exit (-1);
-    }
-    printf("Shared memory has been released.\n");
-
-    /* this releases the semaphore numbered "0" the first in the set*/
-	  semctl_arg.val = 0;
-    semnum = 0;
-    status = semctl(semid, semnum, IPC_RMID, semctl_arg);
-    if (status < 0){
-	     printf( "error in releasing semaphore");
-		   exit(1);
-	  }
-    else printf("Semaphore has been released");
+    ////////////////Old///////////
 }
 
-/*
-   Increased buffer size for processing client info using forloops
-   taking response using forloops and passing within buffer.
-   need to add Mutex or Semaphores and figure out second client.
-   Additionaly need to recieve updated matrix from server before mutex or
-   semiphre swap to maintain correct state when entering critical section.
-*/
-void doprocessing (int sock) {
+void doprocessing (int sock, int pid) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
     int status;
-    char buffer[255];
-    //bzero(buffer,256);
+    bzero(buffer,256);
     //Loop to keep socket running until correct input is recieved
     int go = 1;
-
     while(go){
-        bzero(buffer,256);
-        status = read(sock,buffer,255);
-        // check to make sure status is good
-        if(status < 0){
-          perror("ERROR reading from socket");
-          exit(1);
-        }
-
-        // Checking to see if all clients are ready
+        status = read(sock,buffer,256);
         if(buffer[0] == 'y'){
-          // Increase the number of clients for each call
-          sharedData->numClients++;
-          printf("%d Clients Connected\n", sharedData->numClients);
-           // Check to see if at least 2 players are ready
-           // first case if not enough players
-          if(sharedData->numClients < 2) status = write(sock, "Waiting on other players.\n", 28);
-          // second case 2 players ready
-          if(sharedData->numClients == 2){
-            printf("Enough Clients Connected\n");
-            status = semop(semid, &Signal, 1);
-          }
-
-          status = semop(semid, &Wait, 1);
-          printf("Starting to write board\n");
-          status = write(sock, sharedData->letterBoard, strlen(sharedData->letterBoard));
-          printf("-------------\n");
-          printf("%s",sharedData->letterBoard);
-          printf("-------------\n");
-          //Sends board in buffer to player
-          //status = write(sock,buffer,255);
-
-          status = semop(semid, &Signal, 1);
-
+            bzero(buffer,256);
+            printf("Im Ready! \n");
+            playerInfo->count++;
+            go = 0;
         }
     }
 
+    //Fills buffer with board.
+    //create sendBoard function;
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            buffer[k] = gameBoard[i][j];
+            k++;
+            if(j == 3){
+                buffer[k] = '\n';
+                k++;
+            }
+        }
+    }
+    k = 0;
+    printf("-------------\n");
+    printf("%s",buffer);
+    printf("-------------\n");
+    //Sends board in buffer to player
+    status = write(sock,buffer,256);
+    //waits for two players.
+    while(playerInfo->count < 1){};
     //Needs to wait for input
-    while(1){
-        status= read(sock,buffer,255);
-        if(isalpha(buffer[0])){
-            break;
-        }
-    }
+     printf("Running game\n");
 
-    if (status < 0) {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
 
-    printf("Here is the Board: \n%s\n",buffer);
-    status= write(sock,buffer,1024);
+     bzero(buffer,256);
+     
+     while(1){
+         pid = wait(&status);
+         rungame(sock);
+         pid = signal(&status);
+     }
 
-    if (status < 0) {
-        perror("ERROR writing to socket");
-        exit(1);
-    }
 }
+
+void rungame(int sock){
+        int status = read(sock, buffer, 256);
+        if(status < 0) printf("READ error\n");
+        char letter = buffer[0];
+        int index = letter - 'a';
+        int i = index/4;
+        int j = index%4;
+        printf("%c -> %d\n",buffer[0], scoreMap[i][j]);
+        gameBoard[i][j] = gameBoardMap[i][j];
+        //changes the board to the players choice successfully for one player so far 
+        //have not checked out multiplayer yet.
+        printBoard(i,j);
+        status = write(sock, buffer, 256);
+        bzero(buffer,256);
+}
+
+void printBoard(int i, int j){
+    printf("----------\n");
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            printf("%c ", gameBoard[i][j]);
+        }
+        printf("\n");
+    }
+    printf("----------\n");
+}
+
