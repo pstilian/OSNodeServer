594e0b8cbb9c06e7c6f2f233131e8e530749428a
Jono Sellier <jonosellier@gmail.com>
2019-11-20 12:12:10 -0500

    i think this is it

---
+++ clientFinal.c
@@ -0,0 +1,166 @@
+// Peter Stilian, Donald Christensen, Jonathan Sellier, Emily Cardella
+
+//Fixed issue with input being repeated
+//Created Print function to clean up code
+
+
+/* this program shows how to create sockets for a client.
+it also shows how the client connects to a server socket.
+and sends a message to it. the server must already be running
+on a machine. The name of this machine must be entered in the function
+gethostbyname in the code below. The port number where the server is listening is
+specified in PORTNUM. This port number must also be specified in the server code.
+ * main program */
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#define PORTNUM  10101 /* the port number that the server is listening to*/
+#define DEFAULT_PROTOCOL 0  /*constant for default protocol*/
+char gameBoard[4][4]=       {{'a','b','c','d'},
+                             {'e','f','g','h'},
+                             {'i','j','k','l'},
+                             {'m','n','o','p'}};
+//redeclered as a global
+char buffer[256];
+
+void endGame(int sock){
+   printf("Game end");
+}
+
+//Print board function
+void printBoard(){
+   int i = 0;
+   int j = 0;
+   for(i = 0; i < 4; i++){
+         for(j = 0; j < 4; j++){
+            printf(" %c ", gameBoard[i][j]);
+         }
+         printf("\n");
+      }
+}
+
+void main()
+{
+   int  port;
+   int  socketid;      /*will hold the id of the socket created*/
+   int  status;        /* error status holder*/
+   //char buffer[256];   /* the message buffer*/
+   struct sockaddr_in serv_addr;
+   struct hostent *server;
+   /* this creates the socket*/
+   socketid = socket (AF_INET, SOCK_STREAM, DEFAULT_PROTOCOL);
+   if (socketid < 0) {
+      printf( "error in creating client socket\n");
+      exit (-1);
+    }
+    printf("created client socket successfully\n");
+   /* before connecting the socket we need to set up the right         values in
+   the different fields of the structure server_addr
+   you can check the definition of this structure on your own*/
+   char host[16];
+   printf("Enter the server name to connect to: (eg osnode02)\n");
+   scanf("%s", &host);
+   server = gethostbyname(host);
+   if (server == NULL){
+      printf(" error trying to identify the machine where the server is running\n");
+      exit(0);
+   }
+   port = PORTNUM;
+/*This function is used to initialize the socket structures with null values. */
+   bzero((char *) &serv_addr, sizeof(serv_addr));
+   serv_addr.sin_family = AF_INET;
+   bcopy((char *)server->h_addr,
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+   serv_addr.sin_port = htons(port);
+   /* connecting the client socket to the server socket */
+   status = connect(socketid, (struct sockaddr *) &serv_addr,
+                            sizeof(serv_addr));
+   if (status < 0){
+      printf( " error in connecting client socket with server    \n");
+      exit(-1);
+   }
+    printf("connected client socket to the server socket \n");
+   /* now lets send a message to the server. the message will be
+   whatever the user wants to write to the server.*/
+
+   /* Read server response */
+   bzero(buffer,256);
+
+   char restartGame[5];
+   int go = 1;
+   //check if user is ready
+   while(go) {
+      printf("press y if you are ready. ");
+      while ((getchar()) != '\n'); 
+      scanf("%c", &buffer);
+      if(buffer[0] == 'y' || buffer[0] == 'Y'){
+         buffer[0] = 'y';
+         status = write(socketid, buffer, 256);
+         status = read(socketid,buffer,256);
+         go = 0;
+      }
+   }
+
+   //bzero(buffer,256);
+   
+   //prints buffer
+   printf("-------------\n");
+   printf("%s\n",buffer);
+   printf("-------------\n");
+   int score = 0; //idk why it jus tworks
+   char checkBuff;
+   status = read(socketid, buffer, 256);
+   go = 1;
+   int currGame = 0;
+   while(go){
+      bzero(buffer,255);
+      while ((getchar()) != '\n');
+      printf("%s\nSelect a Letter: \n", buffer);
+      scanf("%c", &buffer);
+      status = write(socketid, buffer, 255);
+      status = read(socketid, buffer, 256);
+
+      if(buffer[0] == 1) {
+         if(buffer[150] > currGame){ //someone won, new game
+            go = 0;
+            score = 0;
+            printf("Game over! Player %d won\n", buffer[151]);
+            currGame = buffer[150];
+            printf("Enter 1 to keep playing: ");
+            scanf("%d", &go);
+         }
+         score+= buffer[1];
+         printf("Score: %d\n", score);
+         int idx;
+         for(idx = 0; idx<16; idx++){
+            gameBoard[idx/4][idx%4] = buffer[100+idx];
+         }
+         printBoard();
+         //int idx;
+         /*
+         for(idx = 0; idx<16; idx++){
+            gameBoard[idx/4][idx%4] = buffer[100+idx];
+         }
+         */
+         //printBoard();
+      } else if(buffer[0] == 'z'){
+            score = 0;
+            go = 0;
+            currGame = buffer[150];
+            //status = read(socketid, buffer, 256);
+            printf("Game over! Player %d won\n", buffer[2]);
+            printf("Enter 1 to keep playing: ");
+            scanf("%d", &go);
+         }
+   }
+   /* this closes the socket*/
+   close(socketid);
+}
+
+
+
---
+++ serverFinal.c
@@ -0,0 +1,306 @@
+// Peter Stilian, Donald Christensen, Jonathan Sellier, Emily Cardella
+
+#include <string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<netdb.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#define PORTNUM  10101 /* the port number the server will listen to*/
+#define DEFAULT_PROTOCOL 0  /*constant for default protocol*/
+#define SHMKEY ((key_t) 7890)
+
+typedef struct
+{
+    int scores[5];
+    int count;
+    int sockArray[5];
+    int sockArrIdx;
+    int totalMoves;
+    char gameBoard[4][4];
+    int gameNum;
+    int pastwinner;
+}shared_mem;
+shared_mem *playerInfo;
+                      
+char gameBoardMap[4][4] =   {{'4','6','2','1'},
+                             {'0','6','2','3'},
+                             {'8','4','9','1'},
+                             {'6','6','8','9'}};
+
+int scoreMap[4][4] =        {{4,6,2,1},
+                             {0,6,2,3},
+                             {8,4,9,1},
+                             {6,6,8,9}};
+
+void printBoard(int i, int j);
+void rungame(int sock, int id);
+void doprocessing (int sock, int pid, int id);
+void announceWinner(int sock);
+char buffer[256];
+
+int winner = 0;
+
+int main( int argc, char *argv[] ) {
+    //int client_size[6];
+    int sockfd, newsockfd, portno, clilen;
+    struct sockaddr_in serv_addr, cli_addr;
+    int status, pid;
+    /////////Added working on SharedMemory /////////
+    int shmid;
+    key_t key = 123; /*shared memory key*/
+    char* shmadd;
+    shmadd = (char*)0;
+
+    if((shmid = shmget (SHMKEY, sizeof(int), IPC_CREAT | 0666)) < 0){
+        perror ("shmget");
+        exit (1);
+    }
+    if ((playerInfo = (shared_mem *) shmat (shmid, shmadd, 0)) == (shared_mem *) -1){
+        perror ("shmat");
+        exit (0);
+    }
+
+    int eye, jay;
+
+    playerInfo->sockArrIdx = 0; //init index of sockets
+    playerInfo->totalMoves = 0; //init moves
+    for(eye = 0; eye<4; eye++){
+        playerInfo->sockArray[eye] = 0;
+        playerInfo->scores[eye] = 0; //init score to 0
+    }
+    playerInfo->gameNum = 0;
+    playerInfo->pastwinner = 0;
+    //Initialized a matrix and testMap that needs ints
+    
+    char letter = 'A';
+    for(eye = 0; eye<4; eye++) for(jay=0; jay<4;jay++){
+        playerInfo->gameBoard[eye][jay] = letter++;
+    }
+    
+    ////////////////End of SharedMemory setup /////////////////////
+    /* First call to socket() function */
+    sockfd = socket(AF_INET, SOCK_STREAM,DEFAULT_PROTOCOL );
+    if (sockfd < 0) {
+        perror("ERROR opening socket");
+        exit(1);
+    }
+    /* Initialize socket structure */
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    portno = PORTNUM;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(portno);
+    /* Now bind the host address using bind() call.*/
+    status =  bind(sockfd, (struct sockaddr *) &serv_addr, sizeof (serv_addr)); 
+    if (status < 0) {
+      perror("ERROR on binding");
+      exit(1);
+    }
+    /* Now Server starts listening clients wanting to connect. No more than 5 
+    clients allowed */
+    listen(sockfd,5);
+    int i = 0;
+    clilen = sizeof(cli_addr);
+    /////////////////old////////////////
+    while (1) {
+       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr,  &clilen);
+       if (newsockfd < 0) {
+         perror("ERROR on accept");
+         exit(1);
+        }
+      /* Create child process */
+        pid = fork();
+        if (pid < 0) {
+            perror("ERROR on fork");
+            exit(1);
+        }
+        if (pid == 0) {
+         /* This is the client process */
+            close(sockfd);
+            doprocessing(newsockfd,pid, playerInfo->sockArrIdx);
+            exit(0);
+        }
+        else {
+            playerInfo->sockArray[playerInfo->sockArrIdx] = pid; //add the socket to the array
+            printf("New player connected\nPIDs of players are: ");
+            for(eye = 0; eye<5; eye++) printf("%d ", playerInfo->sockArray[eye]);
+            printf("\n");
+            close(newsockfd);
+            playerInfo->sockArrIdx++;
+        }
+    } /* end of while */
+    ////////////////Old///////////
+}
+
+void doprocessing (int sock, int pid, int id) {
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int status;
+    bzero(buffer,256);
+    //Loop to keep socket running until correct input is recieved
+    int go = 1;
+    while(go){
+        status = read(sock,buffer,256);
+        if(buffer[0] == 'y'){
+            bzero(buffer,256);
+            printf("Im Ready! \n");
+            playerInfo->count++;
+            buffer[0] = 0;
+            status = write(sock,buffer,256);
+            go = 0;
+        }
+    }
+    //Fills buffer with board.
+    //create sendBoard function;
+
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            buffer[k] = playerInfo->gameBoard[i][j];
+            k++;
+            if(j == 3){
+                buffer[k] = '\n';
+                k++;
+            }
+        }
+    }
+    k = 0;
+    printf("-------------\n");
+    printf("%s",buffer);
+    printf("-------------\n");
+    //Sends board in buffer to player
+    status = write(sock,buffer,256);
+    //waits for two players.
+    while(playerInfo->count < 2){};
+    //Needs to wait for input
+     printf("Running game\n");
+
+     bzero(buffer,256);
+     while(1){
+         pid = wait(&status);
+         rungame(sock, id);
+         pid = signal(&status);
+     }
+}
+void rungame(int sock, int id){
+        int status = read(sock, buffer, 256);
+        if(status < 0) printf("READ error\n");
+        char letter = buffer[0];
+        int index = letter - 'a';
+        int i = index/4;
+        int j = index%4;
+        printf("%c -> %d\n",buffer[0], scoreMap[i][j]);
+        bzero(buffer, 256);
+        buffer[150] = playerInfo->gameNum;
+        buffer[0] = 1;
+        if(playerInfo->gameBoard[i][j]!=gameBoardMap[i][j]){
+            buffer[1] = scoreMap[i][j]; //add score to buffer
+            playerInfo->scores[id] +=scoreMap[i][j]; //increase player score
+        }
+        else{
+            playerInfo->totalMoves--;
+            buffer[1]=0; //if already selected no change in score
+        }
+        int idx;
+        printf("----Game %d-----\nCurrent Scores:\nPID\t|Score\n", playerInfo->gameNum);
+        for(idx = 0; idx<5;idx++) printf("%d\t|%d\n", playerInfo->sockArray[idx], playerInfo->scores[idx]);
+        printf("\n");
+        playerInfo->gameBoard[i][j] = gameBoardMap[i][j];
+        //send gameBoard to buffer
+        
+        for(idx = 0; idx<16; idx++){
+            buffer[100+idx] = playerInfo->gameBoard[idx/4][idx%4];
+        }
+        //Increments moves, had it origonaly in shared memory, however we ran into 
+        //unnown issues, so i made playerInfo->totalMovesoves globalto resolve problem.
+        playerInfo->totalMoves++;
+        if(playerInfo->totalMoves >= 16){
+            buffer[0] = 'z';
+            int pastScore = -1;
+            for(i = 0; i < 5; i++){
+                if(playerInfo->scores[i] > pastScore){
+                    pastScore = playerInfo->scores[i];
+                    winner = i;
+                }
+            }
+            buffer[2] = winner;
+            playerInfo->pastwinner = winner;
+            playerInfo->gameNum++;
+            
+            printf("Resetting...\n");
+        int eye, jay;
+        char letter = 'A';
+        for(eye = 0; eye<4; eye++) for(jay=0; jay<4;jay++){
+            playerInfo->gameBoard[eye][jay] = letter++;
+        }
+        for(eye = 0; eye < 5; eye++)playerInfo->scores[eye]=0;
+        playerInfo->totalMoves = 0;
+
+        }
+        buffer[151] = playerInfo->pastwinner;
+        
+        //totalMoves++;
+        //changes the board to the players choice successfully for one player so far 
+        //have not checked out multiplayer yet.
+        printBoard(i,j);
+        status = write(sock, buffer, 256);
+        bzero(buffer,256);
+        
+        //announceWinner(sock);
+        //printf("\nPlayer info: %d", playerInfo->totalMoves);
+}
+
+void printBoard(int i, int j){
+    printf("----------\n");
+    for(i = 0; i < 4; i++){
+        for(j = 0; j < 4; j++){
+            printf("%c ", playerInfo->gameBoard[i][j]);
+        }
+        printf("\n");
+    }
+    printf("----------\n");
+}
+
+    
+void announceWinner(sock){
+    //todo
+    //Keeps record of scores and player moves,
+    //if playerMoves > 15 then a winner is named
+    //and a message is sent.
+    //Function is made to run  in a loop with its own internal conditional check.
+    int i = 0;
+    int pastScore = 0;
+    int winner = 0;
+    int status = 0;
+    char newBuff[14] = "Player i wins!";
+    printf("Player Move number: %d \n", playerInfo->totalMoves);
+    if(playerInfo->totalMoves > 15){
+        
+        for(i = 0; i < 5; i++){
+            if(playerInfo->scores[i] > pastScore){
+                pastScore = playerInfo->scores[i];
+                winner = i;
+            }
+        }
+        newBuff[7] = '0' + winner;
+        printf("\n  --PLAYER %d WINS-- \n", winner);
+        printf("Resetting...\n");
+        int eye, jay;
+        char letter = 'A';
+        for(eye = 0; eye<4; eye++) for(jay=0; jay<4;jay++){
+            playerInfo->gameBoard[eye][jay] = letter++;
+        }
+        for(eye = 0; eye < 5; eye++)playerInfo->scores[eye]=0;
+        playerInfo->totalMoves = 0;
+        //printf("%s", newBuff);
+        //status = write(sock,newBuff,256);
+    }
+}
+
